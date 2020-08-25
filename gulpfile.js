4930b435290c5dbb8c6268d12812e7baa33a8115
Leo1305Pineda <leo1305pineda@gmail.com>
2020-08-25 03:19:11 -0400

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
+var gm = require('gulp-gm');
+var argv = require('yargs').argv;
  
+/*
+ *usege gulp imagemin --path 'storage/app/public/products/*' --dest products
+ */
 gulp.task('imagemin' , function() {
-    return gulp.src('image/**/*')
+	const base = 'storage/app/public/';
+	const dest = argv.dest || 'products';
+	const src = argv.path || `${base}${dest}/*`;
+    return gulp.src(src)
+	    .pipe(gm(function (gmfile) {
+	      return gmfile.resize(argv.width || 1200, argv.height || 1200);
+	    }))
+	    /*.pipe(gm(function (gmfile, done) {
+	    	gmfile.size(function (err, size) {
+	      		done(null, gmfile.rotate('transparent', size.width < size.height ? -90 : 0));
+	    	});
+	    }))*/
         .pipe(imagemin())
-        .pipe(gulp.dest('image'))
+        .pipe(gulp.dest(`${base}${dest}`))
 });
